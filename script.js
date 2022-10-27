bfe57829a525b6a1c6bc68b1d0b546d65f47e214
daydaywell <random@gmail.com>
2022-10-27 16:04:59 +0800

    test

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ ui.layout(
           <Switch id="airportFloatySwitch" w="auto"/>
         </horizontal> */}
         <horizontal padding="10 10" gravity="center_vertical" w="*">
-          <text layout_weight="1"  w="auto" text="version: 0.45 (stable)" />
+          <text layout_weight="1"  w="auto" text="version: 0.46 (stable)" />
         </horizontal>
         {/* <horizontal padding="10 10">
           <text >提交间隔时间</text><input id="submitIntervalInput" gravity="center" margin="10 0" w="100"></input><text>秒</text>
